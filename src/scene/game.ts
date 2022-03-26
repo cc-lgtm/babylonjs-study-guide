4bf7b2c57db57c67d835e74c56c76446e8d624b3
cc-lgtm <2448856797@qq.com>
2022-03-26 10:35:59 +0800

    修改相机

--- src/scene/game.ts
+++ src/scene/game.ts
@@ -12,19 +12,41 @@ import {
   AmmoJSPlugin,
   OimoJSPlugin,
   FreeCamera,
-  FollowCamera
+  SceneLoader,
+  FollowCamera,
+  UniversalCamera
 } from '@babylonjs/core'
+import '@babylonjs/loaders'
 import { Player, onKeyBord } from '../player'
+
+
+function loadScene(scene: Scene) {
+  SceneLoader.ImportMeshAsync(null, 'src/assets/module/', 'envSetting.glb', scene).then(result => {
+    const mesh = result.meshes[0]
+  })
+}
 
 export function gameScene(engine: Engine, canvas: HTMLCanvasElement): Scene {
   const scene = new Scene(engine)
   const light = new HemisphericLight('light', new Vector3(1, 1, 1), scene)
 
   const player = new Player(scene)
+
+  loadScene(scene)
+
   player.mesh().then(mesh => {
-    const camera = new FollowCamera('camera', new Vector3(10, 10, 11), scene, mesh)
+    const camera = new FollowCamera('camera', new Vector3(10, 10, 5), scene, mesh)
     camera.setTarget(Vector3.Zero())
     camera.attachControl(true)
+    camera.lowerRadiusLimit = 10
+    camera.upperRadiusLimit = 10
+    camera.heightOffset = 10
+    camera.rotationOffset = 0
+
+    mesh.position.x = 70
+    mesh.position.y = 13.44
+    mesh.position.z = 135
+    mesh.rotation = Vector3.Zero()
   })
   player.land()
 
@@ -34,7 +56,6 @@ export function gameScene(engine: Engine, canvas: HTMLCanvasElement): Scene {
     inputMap['w'] && player.run()
     inputMap['s'] && player.dash()
   })
-
 
   return scene
 }
